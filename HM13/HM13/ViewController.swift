6665a6373f6048e548e0bb2c9bea5f7d39e8d049
EvgenSakun <evgen.sakun20@gmail.com>
2024-01-08 21:53:24 +0300

    Start

---
+++ HM13/HM13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM13
+//
+//  Created by Evgeny Sakun on 8.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
